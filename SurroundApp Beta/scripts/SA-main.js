deec2b7aa2ec043f54f7c06321477e05ec77f2d9
david@metatron.com.hk <david@metatron.com.hk>
2013-03-19 02:39:31 -0500

    er

--- SurroundApp Beta/scripts/SA-main.js
+++ SurroundApp Beta/scripts/SA-main.js
@@ -28,10 +28,10 @@ function onDeviceReady() {
 function callWBlogin() {
     var resultMessageElem = document.getElementById('result');
 
- /*     
+ /*      */ 
     closeWBlogin('http://www.surroundapp.asia/postlogin.html#access_token=2.002M91_DwMN8HCef97eb8c5c2KdMQC&remind_in=6345654254&expires_in=250327040&refresh_token=REFRESH_TOKEN');
     return;
-   */
+ 
     objWP = window.plugins;
     var navUri = 'https://api.weibo.com/oauth2/authorize?client_id=1942185646&response_type=token&redirect_uri=http://www.surroundapp.asia/postlogin.html&display=mobile';
     if(objWP != null) {
@@ -47,7 +47,7 @@ function closeWBlogin(redirUrl) {
        
     if (redirUrl.indexOf("redirect_uri") == -1 && redirUrl.indexOf("surroundapp.asia") >= 0) {
         var resultMessageElem = document.getElementById('result');
-        window.plugins.childBrowser.close();
+        //window.plugins.childBrowser.close();
         
         // finding the parameters
         var arrMatches = redirUrl.match(/access_token=([0-9a-zA-Z.=_]*)?/);
@@ -77,45 +77,36 @@ function showAccessToken(e) {
    
   
 function viewInit(e){
-    console.log('viewinit called');
     console.log( window.localStorage.getItem("WBtoken"));
     $("#statusListView").kendoMobileListView({
-    dataSource: new kendo.data.DataSource({
-                  transport: {
-                      read: {
-                       url:"https://api.weibo.com/2/statuses/friends_timeline.json",
-                       dataType: "jsonp", //jsonp, odata
-                       data: {
-                           access_token: window.localStorage.getItem("WBtoken"),
-                           page:1,
-                           count:20 
-                        }
-                       },
-                      beforeSend: function() {
+        dataSource: new kendo.data.DataSource({
+              transport: {
+                  read: {
+                   url:"https://api.weibo.com/2/statuses/friends_timeline.json",
+                   dataType: "jsonp", //jsonp, odata
+                   data: {
+                       access_token: window.localStorage.getItem("WBtoken"),
+                       page:1,
+                       count:20 
+                    },
+                    beforeSend: function() {
                         app.showLoading();         
-                        },
-                      complete: function() {
+                    },
+                    complete: function() {
                         app.hideLoading();
-                        },
-                       error: function(e) {
-                         console.log(e.status);
-                        }
-                      },
-                   schema: {
-                   data: function(response) {
+                    }
+                  }},
+                  schema: {
+                       data: function(response) {
                        return response.data.statuses;
-                   },
-                   errors: function(response) {
-                       // console.log(e.status);
-                       return response.errors;
-                   }
-               }
+                  }
+                 }
+              
     }),
     pullToRefresh: true,
     template: $("#status-template").html()
     });
-    console.log('viewinit ended');
-    }
+}
   
    
    $("#statusListView").kendoMobileListView({
@@ -153,6 +144,12 @@ function viewInit(e){
                           
           }
     });
+
+function displayDetail(e) {
+    console.log(e.view.params.postid);
+    
+    
+}
 
 //============================ Utilities =========================//
 
