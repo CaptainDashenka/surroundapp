f8916eec60921e50a8626fe4548521662d8a94d7
david@metatron.com.hk <david@metatron.com.hk>
2013-03-20 05:21:14 -0500

    dfgdf

--- SurroundApp Beta/scripts/SA-main.js
+++ SurroundApp Beta/scripts/SA-main.js
@@ -6,7 +6,7 @@ document.addEventListener("deviceready", onDeviceReady, false);
 // PhoneGap is ready
 function onDeviceReady() {
    navigator.splashscreen.hide();
-
+        
     // check for storage funcionality and already existing access tokens
     if (checkStorage) {
     var sToken = window.localStorage.getItem("WBtoken");
@@ -28,10 +28,10 @@ function onDeviceReady() {
 function callWBlogin() {
     var resultMessageElem = document.getElementById('result');
 
- /*      */ 
+ /*      
     closeWBlogin('http://www.surroundapp.asia/postlogin.html#access_token=2.002M91_DwMN8HCef97eb8c5c2KdMQC&remind_in=6345654254&expires_in=250327040&refresh_token=REFRESH_TOKEN');
     return;
- 
+ */ 
     objWP = window.plugins;
     var navUri = 'https://api.weibo.com/oauth2/authorize?client_id=1942185646&response_type=token&redirect_uri=http://www.surroundapp.asia/postlogin.html&display=mobile';
     if(objWP != null) {
@@ -47,7 +47,7 @@ function closeWBlogin(redirUrl) {
        
     if (redirUrl.indexOf("redirect_uri") == -1 && redirUrl.indexOf("surroundapp.asia") >= 0) {
         var resultMessageElem = document.getElementById('result');
-        //window.plugins.childBrowser.close();
+        window.plugins.childBrowser.close();
         
         // finding the parameters
         var arrMatches = redirUrl.match(/access_token=([0-9a-zA-Z.=_]*)?/);
@@ -73,9 +73,8 @@ function showAccessToken(e) {
 
 //=================== Time line data feed =========================//
     
-  //var dataSource = 
-   
-  
+var diPost;
+
 function viewInit(e){
     console.log( window.localStorage.getItem("WBtoken"));
     $("#statusListView").kendoMobileListView({
@@ -95,20 +94,24 @@ function viewInit(e){
                     complete: function() {
                         app.hideLoading();
                     }
-                  }},
-                  schema: {
-                       data: function(response) {
-                       return response.data.statuses;
-                  }
-                 }
+        }},
+        schema: {
+            data: function(response) {
+                return response.data.statuses;
+            }
+        }
               
     }),
     pullToRefresh: true,
-    template: $("#status-template").html()
+    template: $("#status-template").html(),
+    click: function(e) {
+        diPost = e.dataItem;
+        window.location = '#itemview';
+    }
     });
 }
   
-   
+   /*
    $("#statusListView").kendoMobileListView({
        //dataSource: dataSource,
        //pullToRefresh: true,
@@ -143,12 +146,36 @@ function viewInit(e){
            window.location = '#detailtweet';
                           
           }
-    });
+    }); */
+
 
 function displayDetail(e) {
-    console.log(e.view.params.postid);
+    //console.log(e.view.params.postid);
+    $('#iv-userscreenname').html(diPost.user.screen_name);
+    $('#iv-profilephoto').attr("src",diPost.user.profile_image_url + '.jpg');
+    $('#iv-username').html(diPost.user.name);
+    $('#iv-posttext').html(diPost.text);
+    $('#iv-postdate').html(kendo.toString(new Date(Date.parse(diPost.created_at)), "ddd dd MMM yyyy HH:mm "));
+  /*  
+     $("#buttongroupstrip").kendoMobileButtonGroup({
+            select: function() {
+                //console.log(this.selectedIndex)
+                if (this.selectedIndex == 0)
+                {}
+                else
+                window.location = '#itemview';
+            },
+            index: 0
+        });
     
+            <script>
+            $("#buttongroupstrip").data("kendoMobileButtonGroup").bind("select", function(e) {
+             console.log(this.selectedIndex)
+               
+         })
+        </script>
     
+    */
 }
 
 //============================ Utilities =========================//
@@ -165,3 +192,47 @@ var checkStorage = (function() {
         return result && storage;
       } catch(e) {}
     }());
+
+
+
+
+         var translationURI = "http://fanyi.youdao.com/openapi.do?keyfrom=SurroundApp&key=231910429&type=data&doctype=jsonp&&callback=some&version=1.1&q=";
+        
+           var fullURI = translationURI + 'original blahblah';
+        
+        
+           
+        
+        //  alert('kcl');
+        //alert(fullURI);
+
+        
+            $.ajax({
+                type: 'GET',   
+                url: fullURI, 
+                dataType: 'jsonp', 
+                jsonpCallback: 'some',
+                success: function (data) {
+                    // $('#translatedbox').html(data.responseData.translatedText);    
+                    // alert('intran');
+                    // alert(data.responseData.translatedText);
+                    
+                    // alert(data.translation)
+                     console.log(data.translation);
+                     //translatedboxtext=data.translation;
+                    //document.getElementById('translatedbox').innerHTML=data.translation;
+                
+                    //alert(translatedboxtext);
+                    //  $('#translatedbox').html(getMessage.toString());
+                    // $('#translatedbox').html('test');
+                
+                    //   $('#translatedbox').append(translatedboxtext.toString());
+                    //alert($('#translatedbox').html(data.responseData.translatedText));
+                }
+                  
+             });
+     
+        //       alert(message);
+        //alert(getMessage);
+
+
